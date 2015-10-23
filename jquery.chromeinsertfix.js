721eddd160cd1c6910e34b0f7095113b7799e7a4
UndeadBaneGitHub <u9262450174@gmail.com>
2015-10-23 20:44:12 +0300

    Added default regex/replace values

--- jquery.chromeinsertfix.js
+++ jquery.chromeinsertfix.js
@@ -1,5 +1,5 @@
 /* jquery.chromeinsertfix
--- version 0.4.2
+-- version 0.5.1
 -- copyright 2014 UndeadBane*2014
 -- licensed under the MIT
 --
@@ -9,12 +9,15 @@
 (function ($) {
     if (typeof ($.fn.chromeinsertfix) !== "function") {
         $.fn.chromeinsertfix = function (stringToReplace, replaceWith) {
+            var _stringToReplace = stringToReplace || new RegExp('(\r\n|\n|\r)', 'gm'),
+                _replaceWith = replaceWith || '';
+            
             this.on("DOMNodeInserted", $.proxy(function (e) {
                 if (e.target.parentNode.getAttribute("contenteditable") === "true") {
                     var newTextNode = document.createTextNode("");
                     function antiChrome(node) {
                         if (node.nodeType == 3) {
-                            newTextNode.nodeValue += node.nodeValue.replace(stringToReplace, replaceWith);
+                            newTextNode.nodeValue += node.nodeValue.replace(_stringToReplace, _replaceWith);
                         }
                         else if (node.nodeType == 1 && node.childNodes) {
                             for (var i = 0; i < node.childNodes.length; ++i) {
